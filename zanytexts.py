40afc165746df4f12a5f30c42cb160bb2a78fdc3
MANASYUC <67393699+MANASYUC@users.noreply.github.com>
2024-12-02 10:59:05 -0600

    Add files via upload

---
+++ zanytexts.py
@@ -0,0 +1,38 @@
+# Manasyu Chaudhari     9-3-2024
+#Assignment 1
+
+#Project 'Zany Text' where the users inputs words creating random stories.
+
+
+print('Zany Texts!\n')
+print('By: Manasyu Chaudhari')
+print('[COM S 1270 2]\n')
+
+
+noun1 = input('noun: ')
+adj1 = input('adjective: ')
+adj2 = input('adjective: ')
+adj3 = input('adjective: ')
+Pverb = input('past tense verb: ')
+noun2 = input('noun: ')
+print('\n')
+print('Zany Text #1\n')
+
+print('There once was a ' + noun1 + '. In its free time it would be ' + adj1 +' a lot. When the ' + noun1+' was with its friends they all would be ' + adj2+
+      '. On the weekends it would be ' + adj3+' on the grass. But when it would eat dinner it would sudden have memories of its childhood come to its mind. ' 
+      + 'The ' + noun2 + ' would also remember that it had ' + Pverb + ' with its family, that family being the '+noun1+'s family. \n')
+
+print('Zany Text #2\n')
+print(noun1 + ' became '+ adj1 + ' every day of the week. ' + noun2 + ' sometimes joins but only when it isnt busy after it '
+      + Pverb + '. This is a problem because when the ' + noun2 + ' wants to be ' + adj2+' the '+noun1+' wants to be '
+      +adj3+'. Thus ends their friendship \n')
+
+print('Zany Text #3\n')
+print('The '+noun1+ ' was always '+adj1+ ' even when things didnt turn out to be nice. The '+ noun2+ ' never was how the '+noun1+ ' was. '
+      +'So '+noun2+ ' started to be more '+adj2+' incase that the '+noun1+ ' would notice. '+ 'However now the '+
+      noun1+' was always being '+ adj3+ '. This caused the '+noun2+ ' to quit being so '+adj2+ ' . The '+noun2+ ' never '+Pverb+ ' again because of this.\n')
+
+print('Zany Text #4\n')
+print('Everyday was an adventure! The '+ noun1 +' and '+noun2+ ' would be '+ adj2+ ' along with their friends. '+ noun1+ ' was always '+adj1+' and '+noun2+' was always '
+      + adj3+' without a question, but when it came to food they both '+Pverb+' towards the smell. They are very weird,'
+      'but the '+noun1 + ' relies on '+noun2+ ' and the '+noun2+' relies on the '+noun1+' (sometimes?)\n')
